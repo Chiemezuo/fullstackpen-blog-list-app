138df822207434737b4e61feec23be984ea55029
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-03-30 15:45:48 +0100

    refactor into blogRouter

--- index.js
+++ index.js
@@ -3,31 +3,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blogModel')
+const blogRouter = require('./routers/blogRouter')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
