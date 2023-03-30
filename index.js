2f4b8e3775af38fbd977ad20037fc989e2bded92
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-03-30 15:54:06 +0100

    refactor into app.js

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
 const mongoose = require('mongoose')
-const blogRouter = require('./routers/blogRouter')
 const { mongoUrl, PORT } = require('./utils/config')
+const app = require('./app')
 
 mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
