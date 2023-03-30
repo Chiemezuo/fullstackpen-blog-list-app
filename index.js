35a99437f6e0f5d89e688b4725f85ae420c0fe00
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-03-30 15:51:08 +0100

    refactor into config.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./routers/blogRouter')
+const { mongoUrl, PORT } = require('./utils/config')
 
-const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
 
-const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
